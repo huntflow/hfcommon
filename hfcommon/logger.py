cdc88010e65fcc151d1d95c19f6d5fb2d420adf5
kalbas <gudakov@huntflow.ru>
2019-12-18 09:12:11 +0300

    DEV-1016: logging configuration with sentry

--- hfcommon/logger.py
+++ hfcommon/logger.py
@@ -1,22 +1,34 @@
 # -*- coding: utf-8 -*-
 import logging
+
+from raven.handlers.logging import SentryHandler
 from tornado.log import access_log
 from tortik.logger import LOGGER_NAME, RequestIdFilter
 
 
-def configure_logging(logfile=None, log_format=None, log_level=logging.WARNING, debug=False):
+def configure_logging(logfile=None, log_format=None, log_level=logging.WARNING, debug=False, sentry_dsn=None):
     log_level = logging.DEBUG if debug else log_level
-    if logfile:
-        handler = logging.handlers.WatchedFileHandler(logfile)
-        handler.setFormatter(logging.Formatter(log_format))
-        for logger in logging.Logger.manager.loggerDict.values():
-            if type(logger) != logging.Logger:
-                continue
 
-            logger.setLevel(log_level)
+    handlers = []
+    if logfile:
+        h = logging.handlers.WatchedFileHandler(logfile)
+        h.setFormatter(logging.Formatter(log_format))
+        handlers.append(h)
+    if sentry_dsn:
+        h = SentryHandler(sentry_dsn, level=logging.ERROR)
+        handlers.append(h)
+    if not handlers:
+        return
+
+    for logger in logging.Logger.manager.loggerDict.values():
+        if type(logger) != logging.Logger:
+            continue
+
+        logger.setLevel(log_level)
+        for handler in handlers:
             logger.addHandler(handler)
-            if logger.name == LOGGER_NAME:
-                logger.addFilter(RequestIdFilter())
+        if logger.name == LOGGER_NAME:
+            logger.addFilter(RequestIdFilter())
 
 
 def tornado_log_function(handler):
